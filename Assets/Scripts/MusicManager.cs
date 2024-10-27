f789767c540db3f3c5c3de72cbdd7d6d75c3d0b2
Joshn't <joshua-hank@web.de>
2024-10-27 12:23:18 +0100

    Audio Confusion (und noch iwas witziges für Dennis, Someone Like you oder so?)

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,65 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using System;
 
 public class MusicManager : MonoBehaviour
 {
-    private AudioSource musicDay;
-    private AudioSource musicDayRain;
-    private AudioSource musicNight;
-    private AudioSource musicNightRain;
+    public AudioMixerSnapshot DaySnapshot;
+    public AudioMixerSnapshot NightSnapshot;    
+    public AudioMixerSnapshot DayRainSnapshot;
+    public AudioMixerSnapshot NightRainSnapshot;
+    public AudioMixerSnapshot PauseSnapshot;
     public float crossfadeDuration = 5.0f;
-    private string currEnvironment = "Day"; //possible states: "Day", "DayRain", "Night", "NightRain"
+    [SerializeField] private string currEnvironment = "Pause"; //possible states: "Day", "DayRain", "Night", "NightRain", "Pause"
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Transform musicDayTransform = transform.Find("musicDay");
-        Transform musicDayRainTransform = transform.Find("musicDayRain");
-        Transform musicNightTransform = transform.Find("musicNight");
-        Transform musicNightRainTransform = transform.Find("musicNightRain");
-        if (musicDayTransform && musicDayRainTransform && musicNightTransform && musicNightRainTransform ){
-            musicDay = musicDayTransform.GetComponent<AudioSource>();
-            musicDayRain = musicNightRainTransform.GetComponent<AudioSource>();
-            musicNight = musicNightTransform.GetComponent<AudioSource>();
-            musicNightRain = musicNightRainTransform.GetComponent<AudioSource>();
-            
-            if (musicDay && musicDayRain && musicNight && musicNightRain)
-            {
-                // set volume of inital environment; if unset, use day
-                switch(currEnvironment){
-                    case "DayRain":
-                        musicDay.volume = 0f;
-                        musicDayRain.volume = 1f;
-                        musicNight.volume = 0f;
-                        musicNightRain.volume = 0f;
-                        break;
-                    case "Night":
-                        musicDay.volume = 0f;
-                        musicDayRain.volume = 0f;
-                        musicNight.volume = 1f;
-                        musicNightRain.volume = 0f;
-                        break;
-                    case "NightRain":
-                        musicDay.volume = 0f;
-                        musicDayRain.volume = 0f;
-                        musicNight.volume = 1f;
-                        musicNightRain.volume = 0f;
-                        break;
-                    default: 
-                        musicDay.volume = 1f;
-                        musicDayRain.volume = 0f;
-                        musicNight.volume = 0f;
-                        musicNightRain.volume = 0f;
-                        currEnvironment = "Day";
-                        break;
-                }
-            } else {
-                Debug.LogWarning("No Audio Sources found on children of music manager.");
+        if (DaySnapshot && NightSnapshot && DayRainSnapshot && NightRainSnapshot)
+        {
+            // set volume of inital environment; if unset, use day
+            switch(currEnvironment){
+                case "DayRain":
+                    DayRainSnapshot.TransitionTo(0.2f);
+                    break;
+                case "Night":
+                    NightSnapshot.TransitionTo(0.2f);
+                    break;
+                case "NightRain":
+                    NightRainSnapshot.TransitionTo(0.2f);
+                    break;
+                case "Pause":
+                    PauseSnapshot.TransitionTo(0.2f);
+                    break;
+                default: 
+                    DaySnapshot.TransitionTo(0.2f);
+                    currEnvironment = "Day";
+                    break;
             }
         } else {
-            Debug.LogWarning("No children found on music manager.");
+            Debug.LogWarning("No Audio Snapshots found on music manager.");
         }
     }
 
@@ -67,54 +47,23 @@ public class MusicManager : MonoBehaviour
         if (currEnvironment == newEnvironment){ 
             return;
         } 
-        StopAllCoroutines();
-        StartCoroutine(CrossfadeCoroutine(newEnvironment));
+        
+        switch(newEnvironment){
+            case "DayRain":
+                DayRainSnapshot.TransitionTo(crossfadeDuration);
+                break;
+            case "Night":
+                NightSnapshot.TransitionTo(crossfadeDuration);
+                break;
+            case "NightRain":
+                NightRainSnapshot.TransitionTo(crossfadeDuration);
+                break;
+            default: 
+                DaySnapshot.TransitionTo(crossfadeDuration);
+                break;
+        }
+
         currEnvironment = newEnvironment;
     }
 
-    private IEnumerator CrossfadeCoroutine(string newEnvironment){
-        float timeStep = 0.05f;
-        float timePassed = 0;
-        AudioSource prevMusic;
-        AudioSource targetMusic; 
-
-        switch(currEnvironment){
-            case "NightRain":
-                prevMusic = musicNightRain;
-                break;
-            case "DayRain":
-                prevMusic = musicDayRain;
-                break;
-            case "Night":
-                prevMusic = musicNight;
-                break;
-            default: 
-                prevMusic = musicDay;
-                break;
-        }
-        switch(newEnvironment){
-            case "NightRain":
-                targetMusic = musicNightRain;
-                break;
-            case "DayRain":
-                targetMusic = musicDayRain;
-                break;
-            case "Night":
-                targetMusic = musicNight;
-                break;
-            default: 
-                targetMusic = musicDay;
-                break;
-        }
-        while (timePassed < crossfadeDuration) {
-            float t = timePassed/ crossfadeDuration;
-
-            prevMusic.volume = Mathf.Lerp(1f, 0f, t);
-            targetMusic.volume = Mathf.Lerp(0f, 1f, t);
-
-            timePassed += timeStep;
-            yield return new WaitForSecondsRealtime(timeStep);
-        }
-
-    }
 }
